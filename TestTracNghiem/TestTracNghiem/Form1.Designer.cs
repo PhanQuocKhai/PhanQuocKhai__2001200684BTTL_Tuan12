3cc40618e5e89a5bacbcb1250bbfb1f7f6576679
QuocKhai <Hantuongtu2102@gmail.com>
2023-11-23 12:31:15 +0700

    add he thong trac nghiem

---
+++ TestTracNghiem/TestTracNghiem/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace TestTracNghiem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rbA = new System.Windows.Forms.RadioButton();
+            this.rbB = new System.Windows.Forms.RadioButton();
+            this.rbC = new System.Windows.Forms.RadioButton();
+            this.rbD = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rbA
+            // 
+            this.rbA.AutoSize = true;
+            this.rbA.Location = new System.Drawing.Point(169, 156);
+            this.rbA.Name = "rbA";
+            this.rbA.Size = new System.Drawing.Size(32, 17);
+            this.rbA.TabIndex = 0;
+            this.rbA.TabStop = true;
+            this.rbA.Text = "A";
+            this.rbA.UseVisualStyleBackColor = true;
+            // 
+            // rbB
+            // 
+            this.rbB.AutoSize = true;
+            this.rbB.Location = new System.Drawing.Point(439, 156);
+            this.rbB.Name = "rbB";
+            this.rbB.Size = new System.Drawing.Size(32, 17);
+            this.rbB.TabIndex = 1;
+            this.rbB.TabStop = true;
+            this.rbB.Text = "B";
+            this.rbB.UseVisualStyleBackColor = true;
+            // 
+            // rbC
+            // 
+            this.rbC.AutoSize = true;
+            this.rbC.Location = new System.Drawing.Point(169, 243);
+            this.rbC.Name = "rbC";
+            this.rbC.Size = new System.Drawing.Size(32, 17);
+            this.rbC.TabIndex = 2;
+            this.rbC.TabStop = true;
+            this.rbC.Text = "C";
+            this.rbC.UseVisualStyleBackColor = true;
+            // 
+            // rbD
+            // 
+            this.rbD.AutoSize = true;
+            this.rbD.Location = new System.Drawing.Point(439, 243);
+            this.rbD.Name = "rbD";
+            this.rbD.Size = new System.Drawing.Size(33, 17);
+            this.rbD.TabIndex = 3;
+            this.rbD.TabStop = true;
+            this.rbD.Text = "D";
+            this.rbD.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(97, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+           
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(310, 314);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(75, 23);
+            this.btnNext.TabIndex = 5;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(723, 395);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rbD);
+            this.Controls.Add(this.rbC);
+            this.Controls.Add(this.rbB);
+            this.Controls.Add(this.rbA);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton rbA;
+        private System.Windows.Forms.RadioButton rbB;
+        private System.Windows.Forms.RadioButton rbC;
+        private System.Windows.Forms.RadioButton rbD;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnNext;
+    }
+}
+
---
+++ TestTracNghiem/TestTracNghiem/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace TestTracNghiem
+{
+    public partial class Form1 : Form
+    {
+        string strCon = "Data Source=A209PC46;Initial Catalog = TRACNGHIEM; Integrated Security = true";
+        public int index = 1;
+        public Form1()
+        {
+            InitializeComponent(); 
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+            SqlConnection consql = new SqlConnection(strCon);
+            string sqlcmd = "Select TenCauHoi, A, B, C, D from De where maCH = @index";
+            SqlCommand cmd = new SqlCommand(sqlcmd,consql);
+            cmd.Parameters.AddWithValue("@index", index);
+            consql.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            if (reader.Read())
+            {
+                label1.Text = reader["TenCauHoi"].ToString();
+                rbA.Text = "A." + reader["A"].ToString();
+                rbB.Text = "B." + reader["B"].ToString();
+                rbC.Text = "C." + reader["C"].ToString();
+                rbD.Text = "D." + reader["D"].ToString();
+            }
+            else
+            {
+                label1.Text = "";
+            }
+        }
+        
+        
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            int id = 1;
+            SqlConnection consql = new SqlConnection(strCon);
+            string sqlcmd = "Select TenCauHoi, A, B, C, D from De where maCH = @index";
+            SqlCommand cmd = new SqlCommand(sqlcmd, consql);
+            cmd.Parameters.AddWithValue("@index", index);
+            if (id != index)
+            {
+                index++;
+                cmd.Parameters.AddWithValue("@index", index);
+                consql.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    label1.Text = reader["TenCauHoi"].ToString();
+                    rbA.Text = "A." + reader["A"].ToString();
+                    rbB.Text = "B." + reader["B"].ToString();
+                    rbC.Text = "C." + reader["C"].ToString();
+                    rbD.Text = "D." + reader["D"].ToString();
+                }
+                else
+                {
+                    label1.Text = "";
+                    rbA.Text = "A.";
+                    rbB.Text = "B.";
+                    rbC.Text = "C.";
+                    rbD.Text = "D.";
+                }
+            }
+        }
+    }
+}
+                
+
